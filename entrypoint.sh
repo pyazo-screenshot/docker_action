90be301208e0ca7e012870a197f72d21ccb57fc8
Jelena Dokic <jelena.dpk@gmail.com>
2020-04-24 18:26:24 +0200

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+REF=$(echo ${{ github.event.client_payload.ref }} | sed "s#\(refs/tags/\)\?v\?##")
+while true; do
+  SHA256=$(curl -sSL https://pypi.org/pypi/pyazo-cli/json | jq -r ".releases[\"$REF\"][1].digests.sha256")
+  if [ "$SHA256" != "null" ]; then
+    break
+  fi
+done
+sed -i "s/^sha256sums=('.\+/sha256sums=('$SHA256/" PKGBUILD
+sed -i "s/^pkgver=('.\+/pkgver=('$REF/" PKGBUILD
+makepkg --printsrcinfo > .SRCINFO
+cat PKGBUILD
