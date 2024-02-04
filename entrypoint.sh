47d4df4596955846ddb6cfe2952fc502a6ebac68
TheEdgeOfRage <git@theedgeofrage.com>
2024-02-04 19:00:00 +0100

    Update pkgbuild with wheel source url

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,14 +24,17 @@ git checkout master
 
 REF=$(echo $1 | sed "s#\(refs/tags/\)\?v\?##")
 while true; do
-  SHA256=$(curl -sSL https://pypi.org/pypi/pyazo-cli/json | jq -r ".releases[\"$REF\"][1].digests.sha256")
-  if [ "$SHA256" != "null" ]; then
-    break
-  fi
+    release=$(curl -sSL https://pypi.org/pypi/pyazo-cli/json | jq -r ".releases[\"$REF\"][0]")
+    SHA256=$(echo $release | jq -r ".digests.sha256")
+    URL=$(echo $release | jq -r ".url")
+    if [ "$SHA256" != "null" ]; then
+        break
+    fi
 done
 
 sed -i "s/^sha256sums=('.\+/sha256sums=('$SHA256')/" PKGBUILD
 sed -i "s/^pkgver='.\+/pkgver='$REF'/" PKGBUILD
+sed -i "s/^source=('.\+/source=('$URL')/" PKGBUILD
 
 cp $PWD/PKGBUILD /tmp
 cd /tmp
